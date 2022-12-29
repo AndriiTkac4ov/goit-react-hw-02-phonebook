30f6ed528d56c622ad463f27d2b354f2805d3853
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-12-29 23:06:35 +0200

    realize all tasks except 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,13 @@
 import { Component } from "react";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
 import { nanoid } from "nanoid";
 
 class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    name: '',
-    number: '',
+    contacts: [],
+    filter: '',
   }
 
   addContact = ({ name, number }) => {
@@ -35,18 +30,34 @@ class App extends Component {
     )
   }
 
+  changeFilter = (event) => {
+    this.setState({filter: event.currentTarget.value})
+  }
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+  }
+
   render() {
+    const filteredContacts = this.getFilteredContacts();
+
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm
           onSubmit={this.addContact}
         />
-
+        
         <h2>Contacts</h2>
-        {/* <Filter ... /> */}
+        <Filter
+          value={this.state.filter}
+          onChange={this.changeFilter}
+        />
         <ContactList
-          contacts={this.state.contacts}
+          contacts={filteredContacts}
           onDelete={this.deleteContact}
         />
       </div>
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,12 @@
+const Filter = ({ value, onChange }) => (
+    <label>
+        Find contacts by name
+        <input
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
+    </label>
+)
+
+export default Filter;
